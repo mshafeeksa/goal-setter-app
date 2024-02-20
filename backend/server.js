e74b77415ec21bbd3619d341277b40bc5218d2d7
Mohammed Shafeek <mail2shafeek.sa@gmai.com>
2024-02-21 01:12:03 +0400

    Bring front end out

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 //serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => res.send('Please set to production'));
 }
